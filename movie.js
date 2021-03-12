2242b0d7cd14baa0cf96c5b9b4ebc015ec1ba46b
asheshpatel65 <78936648+asheshpatel65@users.noreply.github.com>
2021-03-11 21:02:34 -0500

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$('input[type="checkbox"]').click(function () {
+ var count=document.querySelectorAll("input:checked").length
+  document.getElementById('no_seat').innerHTML = '';
+	document.getElementById('no_seat').innerHTML = count;
+
+});
+ 
+});
+
+
+function selectmovie() {
+
+	var a = document.getElementById("moviename").value;
+	document.getElementById('movie_name').innerHTML = '';
+	document.getElementById('movie_name').innerHTML = a;
+	if(a=="MS Dhoni")
+	{
+document.getElementById('price').innerHTML = '';
+	document.getElementById('price').innerHTML = "9$";
+  document.getElementById('no_seat').innerHTML = '';
+
+	}
+	else if(a=="Milkha singh")
+	{
+document.getElementById('price').innerHTML = '';
+	document.getElementById('price').innerHTML = "6$";
+  document.getElementById('no_seat').innerHTML = '';
+
+	}
+	else{
+		document.getElementById('price').innerHTML = '';
+	document.getElementById('price').innerHTML = "3$";
+  document.getElementById('no_seat').innerHTML = '';
+
+	}
+}
